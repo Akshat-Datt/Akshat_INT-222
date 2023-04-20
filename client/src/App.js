109a18706972fedeadc4e06f27ef92abad22f1b6
akshat <dattakshat31@gmail.com>
2023-04-20 15:35:47 +0530

    1st

---
+++ client/src/App.js
@@ -0,0 +1,52 @@
+import { useState, useEffect, useRef } from 'react';
+import './App.css';
+import { uploadFile } from './service/api';
+
+function App() {
+  const [file, setFile] = useState('');
+  const [result, setResult] = useState('');
+
+  const fileInputRef = useRef();
+
+  const url = 'https://store-images.s-microsoft.com/image/apps.19837.13583281918024701.b2e73c8e-4b18-4a37-b5a1-da8ea78b4bc2.1aa8a02c-dd65-4543-a3ec-9b60d48003cf';
+
+  useEffect(() => {
+    const getImage = async () => {
+      if (file) {
+        const data = new FormData();
+        data.append("name", file.name);
+        data.append("file", file);
+
+        const response = await uploadFile(data);
+        setResult(response.path);
+      }
+    }
+    getImage();
+  }, [file])
+
+  const onUploadClick = () => {
+    fileInputRef.current.click();
+  }
+
+  return (
+    <div className='container'>
+      <img src={url} className='img' />
+      <div className='wrapper'>
+        <h1>FILE SHARING APP</h1>
+        <p>Upload and share the download link.</p>
+        
+        <button onClick={() => onUploadClick()}>Upload</button>
+        <input
+          type="file"
+          ref={fileInputRef}
+          style={{ display: "none" }}
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+
+        <a href={result} target='_blank'>{result}</a> 
+      </div>
+    </div>
+  );
+}
+
+export default App;
